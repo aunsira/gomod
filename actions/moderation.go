694ca716db7135e0bbeb5cd56af0d9b328d9de0f
aun <aun.sira@gmail.com>
2021-10-11 14:52:42 +0700

    feat: draft

---
+++ actions/moderation.go
@@ -0,0 +1,149 @@
+package actions
+
+import (
+	"errors"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/aunsira/gomod/config"
+)
+
+const (
+	ModerationPath = "/api/v1/moderations"
+)
+
+// Example:
+//
+//  imgData, get := &gomod.GetModeration{}, &actions.GetModeration{
+//      ID: "5a546e916e11571f570c1533",
+//  }
+//
+//  if err := client.Call(imgData, get); err != nil {
+//      log.Fatal(err)
+//  }
+//
+//  fmt.Printf("AI Consensus: %#v\n", imgData)
+//
+type GetModeration struct {
+	ID string
+}
+
+// Example:
+//
+//  list, get := &gomod.GetModerations{}, &actions.GetModerations{
+//      ID: "5a546e916e11571f570c1533",
+//      Page: 1,
+//      Item: 20,
+//  }
+//
+//  if err := client.Call(list, get); err != nil {
+//      log.Fatal(err)
+//  }
+//
+//  fmt.Printf("AI Consensus: %#v\n", list)
+//  fmt.Printf("First element: %#v\n", list.Data.Images[0])
+//
+type GetModerations struct {
+	ID   string
+	Page string
+	Item string
+}
+
+// Example:
+//
+//  imgData, post := &gomod.PostModeration{}, &actions.PostModeration{
+//		Data: TestImageDataURL,
+//  }
+//
+//  if err := client.Call(imgData, post); err != nil {
+//      log.Fatal(err)
+//  }
+//
+//  fmt.Printf("AI Consensus: %#v\n", imgData)
+//
+type PostModeration struct {
+	Data           string
+	PostbackURL    string
+	PostbackMethod string
+	CustomID       string
+}
+
+// Endpoint returns K Sequencing's request url, verb and endpoint for calling Get AI
+// Consensus API.
+func (g *GetModeration) Endpoint() (string, string, string) {
+	return config.KiyoImageAPIURL, "GET", ModerationPath
+}
+
+// Endpoint returns K Sequencing's request url, verb and endpoint for calling Get list of
+// AI Consensus API.
+func (g *GetModerations) Endpoint() (string, string, string) {
+	return config.KiyoImageAPIURL, "GET", ModerationPath
+}
+
+// Endpoint returns K Sequencing's request url, verb and endpoint for calling Create AI
+// Consensus API.
+func (p *PostModeration) Endpoint() (string, string, string) {
+	return config.KiyoImageAPIURL, "POST", ModerationPath
+}
+
+// Payload creates request's payload for Get AI Consensus API. Returns http.Request
+// object which contains required query parameters.
+func (g *GetModeration) Payload(endpoint, method, path string) (*http.Request, error) {
+	if g.ID == "" {
+		return nil, errors.New("ID is required ")
+	}
+
+	req, err := http.NewRequest(method, string(endpoint)+path+"/"+g.ID, nil)
+	if err != nil {
+		return nil, err
+	}
+	return req, nil
+}
+
+// Payload creates request's payload for Get list AI Consensus API. Returns
+// http.Request which contains required query parameters.
+func (g *GetModerations) Payload(endpoint, method, path string) (*http.Request, error) {
+	req, err := http.NewRequest(method, string(endpoint)+path, nil)
+	if err != nil {
+		return nil, err
+	}
+	q := req.URL.Query()
+	if g.ID != "" {
+		q.Add("id", g.ID)
+	}
+	if g.Page != "" {
+		q.Add("page", g.Page)
+	}
+	if g.Item != "" {
+		q.Add("per_page", g.Item)
+	}
+	req.URL.RawQuery = q.Encode()
+	return req, nil
+}
+
+// Payload creates request's payload for Create AI Consensus API. Returns
+// http.Request which contains required query parameters.
+func (p *PostModeration) Payload(endpoint, method, path string) (*http.Request, error) {
+	values := url.Values{}
+	if p.Data != "" {
+		values.Set("data", p.Data)
+	}
+	if p.PostbackURL != "" {
+		values.Set("postback_url", p.PostbackURL)
+	}
+	if p.PostbackMethod != "" {
+		values.Set("postback_method", p.PostbackMethod)
+	}
+	if p.CustomID != "" {
+		values.Set("custom_id", p.CustomID)
+	}
+
+	body := strings.NewReader(values.Encode())
+	req, err := http.NewRequest(method, string(endpoint)+path, body)
+	if err != nil {
+		return nil, err
+	}
+
+	return req, nil
+}
---
+++ config/endpoint.go
@@ -0,0 +1,6 @@
+package config
+
+const (
+	// PosmoniAPIURL represents the Kiyo-Image endpoint API.
+	PosmoniAPIURL string = "https://api.posmoni.com"
+)
